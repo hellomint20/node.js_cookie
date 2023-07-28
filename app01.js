73884736a4f2c72321ab56a67a5143525743dbf9
hellomint20 <121654629+hellomint20@users.noreply.github.com>
2023-07-28 17:13:33 +0900

    app01.js session, body 추가

--- app01.js
+++ app01.js
@@ -2,15 +2,22 @@ const express = require('express');
 const cookieRouter = require('./src/routers/cookie/cookie_router');
 const cookieParser = require('cookie-parser');
 
+const sessionRouter = require("./src/routers/session/session_router");  //router 가져옴
+const session = require('express-session'); //npm 가져옴
+const sessionConfig = require("./config/cookie_session/config");    //config 가져옴
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.use(cookieParser()); 
-app.use("/cookie", cookieRouter); 
+app.use(cookieParser("secret")); 
+app.use(session(sessionConfig.sessionConfig )); //sessionCofing 로 session 사용 선언
+app.use( bodyParser.urlencoded ({extended: true}) );
 
-//웹 통신할 때 쿠키 사용 가능
+app.use("/cookie", cookieRouter); 
+app.use("/session", sessionRouter); 
 
 app.listen(3000, (() => {console.log("cookie server success!!!"); }));
 
