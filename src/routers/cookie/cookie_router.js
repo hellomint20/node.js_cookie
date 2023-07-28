9862a8a5f78edf74848c019fb9e41de95f8d86d5
hellomint20 <121654629+hellomint20@users.noreply.github.com>
2023-07-28 17:14:56 +0900

    save, list 추가

--- src/routers/cookie/cookie_router.js
+++ src/routers/cookie/cookie_router.js
@@ -7,5 +7,11 @@ router.get('/popup', ctrl.popup);
 router.get('/quiz', ctrl.quiz);
 router.get('/quiz_popup', ctrl.quizPopup);
 router.get('/makeCookie', ctrl.makeCookie);
+router.get('/cart', ctrl.cart);
+router.get('/save1', ctrl.save1);
+router.get('/save2/:goods', ctrl.save2);
+                //:<변수> '/' 로 경로를 만드는 경우 변수를 만들어서 받아줘야 함
+                //'?' 로 경로를 만드는 경우 param으로 값을 꺼내오면 됨
+router.get('/view_list', ctrl.viewList);
 
 module.exports = router;
